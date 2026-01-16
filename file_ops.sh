cbc2e9c35be78872cbc89b38155800ebd64358e6
malathi-shetty <shettymalathi113@gmail.com>
2026-01-16 15:48:40 +0530

    Restore local changes to file_ops.sh

--- file_ops.sh
+++ file_ops.sh
@@ -1,14 +1,3 @@
-#!/bin/bash
-
-# File and directory operations
-
-DIR="demo_dir"
-
-echo "Creating directory..."
-mkdir -p $DIR
-
-echo "Creating file..."
-touch $DIR/sample.txt
-
-echo "Listing directory contents:"
-ls -l $DIR
+# File operations examples
+echo Listing files in current directory
+ls -l
